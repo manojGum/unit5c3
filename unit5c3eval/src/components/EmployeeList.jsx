273eba56a0f782ad69c1092143a75f75f1bdd72c
manojGum <Manojgum@gmail.com>
2022-04-25 16:59:12 +0530

    done

--- unit5c3eval/src/components/EmployeeList.jsx
+++ unit5c3eval/src/components/EmployeeList.jsx
@@ -1,12 +1,30 @@
+import { useEffect ,useState } from "react";
+import axios from "axios";
+import{Link} from "react-router-dom"
+
 export const EmployeeList = () => {
-  return (
-    <div className="list_container">
-      {/* On clicking this card anywhere, user goes to user details */}
-      <div className="employee_card">
-        <img className="employee_image" />
-        <span className="employee_name"></span>
-        <span className="employee_title"></span>
+
+  const [data,setdata] = useState([])
+
+  useEffect(()=>{
+    axios.get("http://localhost:8080/employee ").then(({data})=>{
+       setdata(data)
+    })
+ },[])
+ //console.log(data)
+    return (
+      <div className="list_container">
+        {data.map((e,i)=>{
+          return (
+          <Link to={`/employees/${e.id}`}>
+          <div className="employee_card" key={i}>
+          <img className="employee_image" src={e.image}/>
+          <span className="employee_name">{e.employee_name}</span>
+          <span className="employee_title">{e.title}</span>
+        </div></Link>
+          )
+        })}
+        
       </div>
-    </div>
-  );
-};
+    );
+  };
